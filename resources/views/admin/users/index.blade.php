554259ab37e78d89b52d14d605d091888cede2d7
Igor Kuna <kunaigor44@gmail.com>
2016-09-01 10:01:57 +0200

    able to persist user data without photos

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -32,27 +32,6 @@
     	@endforeach
 
 
-
-
-
-{{-- 
-      <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>john@example.com</td>
-      </tr>
-      <tr>
-        <td>Mary</td>
-        <td>Moe</td>
-        <td>mary@example.com</td>
-      </tr>
-      <tr>
-        <td>July</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
-      </tr>--}}
-
-
     </tbody>
   </table>
 @endsection
